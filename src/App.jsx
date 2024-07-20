8e04112ed774e8d5b699c10318495687d927580d
Zokirjon Murotov <zokirmurotov863@gmail.com>
2024-07-20 19:28:31 +0500

    added p tag

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div>
     <h1>Hi Users👋</h1>
+    <p>Welcome to my website! I'm a full-stack developer and I'm here to help you with your coding needs.</p>
     </div>
   )
 }
