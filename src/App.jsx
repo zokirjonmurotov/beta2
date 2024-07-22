0c5e41647eea427976cafaac46c18dcb2445d00a
Zokirjon Murotov <zokirmurotov863@gmail.com>
2024-07-22 20:03:57 +0500

    added some hello texts to app and about pages

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ function App() {
         <Route path='/about' element={<About/>}/>
         <Route path='*' element={<NotFound/>}/>
       </Routes>
+      <h2>Hello hackers</h2>
     </div>
   )
 }
--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div>
+            <h2>update</h2>
             <h1>About page</h1>
         </div>
     );
